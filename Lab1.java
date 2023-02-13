e7d48eb0afccb2ece214ec9b2566ba38c7e58cdd
Sandeep Moorani <sandeepmoorani222@gmail.com>
2023-02-13 15:46:42 +0500

    first commit

---
+++ Lab1.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Lab 01 Task-1 
+
+// public class Lab1 {
+//     public static void main(String[] args) {
+//         int age = 29;
+//         System.out.println(age);
+//         boolean isManager = true;
+//         System.out.println(isManager);
+//         double yearOfService = 2.5;
+//         System.out.println(yearOfService);
+//         int baseSalary = 3000;
+//         int overTime = 40;
+//         int totalPayment = baseSalary + overTime;
+//         System.out.println(totalPayment);
+//         double firstBouns = 10.0, secontbonus = 22.0, thirdbonus = 35.0;
+//         double sum = firstBouns + secontbonus + thirdbonus;
+//         System.out.println(sum);
+
+//     }
+// }
+
+// Lab 01 Task-2 
+
+// public class Lab1 {
+//     public static void main(String[] args) {
+
+// String menuTitle="my Dream Menu";
+// System.out.println(menuTitle);
+
+// ArrayList<String>menu=new ArrayList<>();
+// String starter="salad";
+// menu.add(starter);
+// String mainCourse="java course";
+// menu.add(mainCourse);
+// String dessert="sweet";
+// menu.add(dessert);
+// System.out.println(menu);
+// }
+// }
+
+// Lab 01 Task-3 
+
+// public class Lab1 {
+//     public static void main(String[] args) {
+// Scanner sc=new Scanner(System.in);
+
+// System.out.println("Enter your age ");
+// int age=sc.nextInt();
+// if(age<13){
+//     System.out.println("ticket price is 5$ ");
+// }
+// else if(age>=13 && age<65){
+//     System.out.println("ticket price is 10$ ");
+// }
+// else if(age>=65){
+//     System.out.println("ticket price is 7$ ");
+// }
+//     }
+// }
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Sandeepmoorani/Practical-DSA.git
+git push -u origin main
+
